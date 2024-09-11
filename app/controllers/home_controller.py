678f8cc92f274d0398a144ff8b44a5b7902d8f22
chdencor <lucasignaciodencor@gmail.com>
2024-09-11 12:59:39 -0300

    correctly setup flask. Made some changes to html template directory

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
 from app.models.user import User
 
-home_bp = Blueprint('home', __name__)
+home_bp = Blueprint('home', __name__, template_folder='../views/templates')
+
+
 
 @home_bp.route('/')
 def index():
