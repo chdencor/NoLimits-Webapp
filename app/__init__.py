c3bc4b9f547ac762b0d14404c70b8e22648d0fe1
tiago cotelo <cotelotiago@gmail.com>
2024-10-12 23:09:50 -0300

    refactorizacion de la pagina web

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from app.controllers.cripto_controller import crypto_bp
 from app.controllers.home_controller import home_bp
-from threading import Thread
-from app.models.data_loader import dataLoader
-from app.models.supabaseDAO import SupabaseDAO
 from app.config import Config
 from dotenv import load_dotenv
 import os
@@ -24,14 +21,5 @@ def create_app():
     # Registrar Blueprints
     app.register_blueprint(home_bp)
     app.register_blueprint(crypto_bp)
-    
-    # Conexión a la base de datos
-    dao_instance = SupabaseDAO(database_url)  # Usar la variable de entorno
-    dao_instance.connect()
-
-    # Iniciar el hilo para el data_loader
-    loader_thread = Thread(target=dataLoader)
-    loader_thread.daemon = True  # El hilo se cerrará cuando se cierre la aplicación
-    loader_thread.start()
 
     return app
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,14 +1,5 @@
 # models/__init__.py
 
-from app.models.supabaseDAO import SupabaseDAO
+
 from app.config import Config
 
-# Crear una instancia del DAO
-dao_instance = SupabaseDAO(Config.DATABASE_URL)
-
-def initialize():
-    """Función para inicializar el DAO."""
-    dao_instance.connect()
-
-# Llama a initialize() al cargar el módulo
-initialize()
